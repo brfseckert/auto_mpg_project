11a2fb5097b9a87b4ab0b001bb6c1cdd94b05257
Felipe <felipeeckert@hotmail.com>
2022-08-23 13:03:12 -0400

    adjusting the model

--- main.py
+++ main.py
@@ -4,25 +4,37 @@ from model.ml_model import predict_mpg
 
 app = Flask('app')
 
-@app.route('/test', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def test():
-    return 'Bom dia!'
+    return render_template('home.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET'])
 def predict():
-    vehicle = request.get_json()
-    print(vehicle)
+    # vehicle = request.get_json()
+    # print(vehicle)
+
+    vehicle= {
+        'Cylinders': [float(request.args.get('cylinders'))],
+        'Displacement': [float(request.args.get('displacement'))],
+        'Horsepower': [float(request.args.get('horsepower'))],
+        'Weight': [float(request.args.get('weight'))],
+        'Acceleration': [float(request.args.get('acceleration'))],
+        'Model Year': [int(request.args.get('model-year'))],
+        'Origin': [int(request.args.get('origin'))],
+    }
+
     with open('./model/model.bin', 'rb') as f_in:
         model = pickle.load(f_in)
         f_in.close()
     predictions = predict_mpg(vehicle, model)
+    predictions = [round(x,2) for x in predictions]
 
     result = {
-        'mpg_prediction': list(predictions)
+        'Miles Per Gallon Prediction': list(predictions)
     }
 
-    return jsonify(result)
+    return render_template('output.html', mpg=result)
 
 #for development, uncomment the session below 
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=9696)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
--- model/ml_model.py
+++ model/ml_model.py
@@ -2,10 +2,28 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.compose import ColumnTransformer
 
-#dictonary to encode the origin column as 
+#dictonary to encode the origin column
 origin_dic = {1: 'India', 2: 'USA', 3 : 'Germany'}
+
+#data for fitting the parameters
+# defining the column names
+cols = ['MPG','Cylinders','Displacement','Horsepower','Weight',
+                'Acceleration', 'Model Year', 'Origin']
+# reading the .data file using pandas
+data = pd.read_csv('./auto-mpg.data', names=cols, na_values = "?",
+                comment = '\t',
+                sep= " ",
+                skipinitialspace=True)
+#making a copy of the dataframe
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(data, data['Cylinders']):
+    train_set = data.loc[train_index]
+
+train_data = train_set.drop('MPG',axis=1)
+
 
 def _encode_origin_column(df):
     df['Origin'] = df['Origin'].map(origin_dic)
@@ -31,6 +49,8 @@ def prepare_data(df):
     
     return df
 
+train_fit_data = prepare_data(train_data)
+
 #normalizing the numeric columns and encoding the categorical ones
 def transform_columns(df):
     numeric_columns = df.select_dtypes(['float64','int64']).columns
@@ -38,18 +58,23 @@ def transform_columns(df):
     
     transformer = ColumnTransformer([
                     ('standard_scalar',StandardScaler(), list(numeric_columns)),
-                    ('one_hot_encoding',OneHotEncoder(),category_columns)
+                    ('one_hot_encoding',OneHotEncoder(categories=[["India", "USA", "Germany"]]),category_columns)
                 ])
-    return transformer.fit_transform(df)
+    transformer = transformer.fit(train_fit_data)
+
+    return transformer.transform(df)
 
 def predict_mpg(data, model):
-    
+
+    print(type(data))
     if type(data) == dict:
         df = pd.DataFrame(data)
     else:
         df = data
         
     prepared_data = prepare_data(df)
+    print(prepared_data)
     model_input = transform_columns(prepared_data)
+    print(model_input)
     
     return model.predict(model_input)
